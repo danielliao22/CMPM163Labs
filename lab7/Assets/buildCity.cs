8fd43ebb690733bed568cc4e295c983a6b4bf737
danielliao22 <43585541+danielliao22@users.noreply.github.com>
2020-05-26 18:23:25 -0700

    Added lights

--- lab7/Assets/buildCity.cs
+++ lab7/Assets/buildCity.cs
@@ -12,21 +12,24 @@ public class buildCity : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         float seed = Random.Range(0, 100);
+        print(seed);
         for(int h = 0; h < mapHeight; h++){
             for(int w = 0; w < mapWidth; w++){
                 int result = (int) (Mathf.PerlinNoise(w/10.0f + seed, h/10.0f + seed)*10);
                 Vector3 pos = new Vector3(w*buildingFootprint, 0, h*buildingFootprint);
-                //loat height = buildings[1].height;
+                float height = buildings[1].height;
                 if(result < 2)
                     Instantiate(buildings[0],pos,Quaternion.identity);
                 else if(result < 4)
                     Instantiate(buildings[1],pos,Quaternion.identity);
-                else if(result < 6)
+                else if(result < 5)
                     Instantiate(buildings[2],pos,Quaternion.identity);
-                else if(result < 8)
+                else if(result < 6)
                     Instantiate(buildings[3],pos,Quaternion.identity);
-                else if(result < 10)
+                else if(result < 7)
                     Instantiate(buildings[4],pos,Quaternion.identity);
+                else if(result < 10)
+                    Instantiate(buildings[5],pos,Quaternion.identity);
             }
         }
         
